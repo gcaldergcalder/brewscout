9a9bd4efc802119fee281c44766ca58762823170
gcalder <gilesoliverc@gmail.com>
2026-01-17 10:14:29 +1100

    dep: add lombok dependency

--- backend/src/main/java/com/brewscout/model/User.java
+++ backend/src/main/java/com/brewscout/model/User.java
@@ -11,8 +11,17 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
 import jakarta.persistence.PreUpdate;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
